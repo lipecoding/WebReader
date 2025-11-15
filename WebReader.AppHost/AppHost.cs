a35d92627b5d28d455816fc6599ebd4a211a1e8a
Felipe Reis <felipe-fleming@hotmail.com>
2025-11-15 15:58:15 -0300

    :tada: - recreate web layer

--- WebReader.AppHost/AppHost.cs
+++ WebReader.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.WebReader_API>("webreader-api");
 
+builder.AddProject<Projects.WebReader_Web>("webreader-web");
+
 builder.Build().Run();
