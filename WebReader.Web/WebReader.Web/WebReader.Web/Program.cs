06a9724deeecf469dec2ffc9ff778feb100915d2
Felipe Reis <felipe-fleming@hotmail.com>
2025-11-15 16:03:14 -0300

    :heavy_plus_sign: - add mudblazor

--- WebReader.Web/WebReader.Web/WebReader.Web/Program.cs
+++ WebReader.Web/WebReader.Web/WebReader.Web/Program.cs
@@ -1,3 +1,4 @@
+using MudBlazor.Services;
 using WebReader.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ builder.AddServiceDefaults();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
